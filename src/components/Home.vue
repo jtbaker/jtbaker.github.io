4239676186b3e4a889110c40db0c27cd798c50ba
Jason Baker <jbaker@freightwaves.com>
2020-05-30 00:00:30 -0400

    Fix dependencies.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,14 +1,15 @@
 <template>
     <v-container grid-list-xs>
-        {{ aboutme }}
+        <!-- {{ aboutme }} -->
+        <span>Test content</span>
     </v-container>
 </template>
 <script lang="js">
 
 
 // declare module 'raw-loader!./resources/aboutme.txt'
-import aboutme from 'raw-loader!./resources/aboutme.txt'
-import marked from 'marked'
+// import aboutme from 'raw-loader!./resources/aboutme.txt'
+// import marked from 'marked'
 
 
 
