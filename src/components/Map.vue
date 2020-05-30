b0cb35f4de8477fb6215731d721b81266182ed2a
Jason Baker <jbaker@freightwaves.com>
2020-05-30 12:42:32 -0400

    Css tweaks

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,49 +1,63 @@
 <template>
-    <div ref="mymap" class="mymap fill-height">
-
+  <div ref="mymap" class="mymap fill-height">
+    <div
+      id="layercontrol"
+      style="z-index: 99999;"
+      class="text-left p-2 m-2 border-black-200 bg-opacity-75"
+    >
+      <h4>Layer Control</h4>
     </div>
+  </div>
 </template>
 <script lang="ts">
-import Vue from 'vue'
-import { Map
-//, LngLatBounds, LngLat, Popup, NavigationControl 
-} from 'mapbox-gl'
-import style from './resources/mapboxstyle'
+import Vue from "vue";
+import {
+  Map,
+  //, LngLatBounds, LngLat, Popup, NavigationControl
+} from "mapbox-gl";
+import style from "./resources/mapboxstyle";
 
 export default Vue.extend({
-    data: () => ({
-        map: {} as Map
-    }),
-    mounted() {
-        const map = this.map = new Map({
-            container: this.$refs.mymap as HTMLElement,
-            style
-            // transformRequest: function(url, resourceType){
-            //     console.log(resourceType)
-            //     return {
-            //         url: url,
-            //         // headers: {
-            //         //     // "a"
-            //         // }
-            //     }
-            // }
-        })
+  data: () => ({
+    map: {} as Map,
+  }),
+  mounted() {
+    const map = (this.map = new Map({
+      container: this.$refs.mymap as HTMLElement,
+      style,
+      // transformRequest: function(url, resourceType){
+      //     console.log(resourceType)
+      //     return {
+      //         url: url,
+      //         // headers: {
+      //         //     // "a"
+      //         // }
+      //     }
+      // }
+    }));
 
-        map.on('load', ()=>{
-            console.log('loaded!')
-        })
-    }
-
-})
+    map.on("load", () => {
+      console.log("loaded!");
+    });
+  },
+});
 </script>
 <style lang="scss" scoped>
-@import '../../node_modules/mapbox-gl/dist/mapbox-gl.css';
+@import "../../node_modules/mapbox-gl/dist/mapbox-gl.css";
 
-.mymap{
-    // display: flex;
-    // flex-grow: inherit;
-    // width: 100%;
-    // height: 100%;
+.mymap {
+  // display: flex;
+  // flex-grow: inherit;
+  // width: 100%;
+  // height: 100%;
 }
 
+.mapboxgl-map {
+  height: 100%;
+}
+
+#layercontrol {
+  width: 20%;
+  position: relative;
+}
 </style>
--- src/components/resources/mapboxstyle.ts
+++ src/components/resources/mapboxstyle.ts
@@ -1,24 +1,24 @@
-import { Style } from 'mapbox-gl'
+import { Style } from "mapbox-gl";
 
 export default {
-    version: 8,
-    center: [-97, 40],
-    zoom: 4,
-    sources: {
-        osm: {
-            type: "raster",
-            tileSize: 256,
-            tiles: [
-                "https://a.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png",
-                "https://b.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
-            ]
-        }
+  version: 8,
+  center: [-97, 40],
+  zoom: 4,
+  sources: {
+    osm: {
+      type: "raster",
+      tileSize: 256,
+      tiles: [
+        "https://a.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png",
+        "https://b.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png",
+      ],
     },
-    layers: [
-        {
-            id: 'osm',
-            type: 'raster',
-            source: 'osm'
-        }
-    ]
-} as Style
+  },
+  layers: [
+    {
+      id: "osm",
+      type: "raster",
+      source: "osm",
+    },
+  ],
+} as Style;
